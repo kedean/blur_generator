e09cedeafec785722404196c6e394241d08527f7
kedean <kevinedean@gmail.com>
2013-04-02 15:02:17 -0500

    switched to a BlurringMatrix class, which now implements the low pass filter

--- blur.py
+++ blur.py
@@ -1,4 +1,5 @@
 import numpy
+import math
 
 class Types:
     PIL = 0
@@ -43,7 +44,7 @@ def loadWithPIL(source):
         raise ImportError("Could not import PIL.")
     else:
         image = Image.open(source)
-        return numpy.array(image, dtype='uint8')
+        return BlurringMatrix(numpy.array(image, dtype='uint8'))
 
 def loadWithPygame(source):
     try:
@@ -53,7 +54,7 @@ def loadWithPygame(source):
     else:
         image = pygame.image.load(source)
         pygame.surfarray.use_arraytype("numpy")
-        return pygame.surfarray.array3d(image)
+        return BlurringMatrix(pygame.surfarray.array3d(image))
     
 def exportToPIL(matrix):
     try:
@@ -61,7 +62,7 @@ def exportToPIL(matrix):
     except:
         raise ImportError("Could not import PIL.")
     else:
-        return Image.fromarray(matrix)
+        return Image.fromarray(matrix.matrix.astype('uint8'))
 
 def exportToPygame(matrix, surface=None):
     try:
@@ -71,23 +72,72 @@ def exportToPygame(matrix, surface=None):
     else:
         if surface is None:
             surface = pygame.Surface(matrix.shape[0:2])
-        pygame.surfarray.blit_array(surface, matrix)
+        pygame.surfarray.blit_array(surface, matrix.matrix.astype('uint8'))
         return surface
 
-
-"""
-Applies a low pass blurring filter, allowing a maximum of the given cycles per degree of visual angle.
-"""
-def lowPassFilter(self, matrix, cyclesPerDegree):
-    pass
-
-
-"""
-Cuts out a circular window of the given radius (in pixels).
-If no position is specified, the window is placed at the image center.
-"""
-def cutWindow(self, matrix, radius, position=None):
-    if position is None:
-        position = BlurGenerator.CENTER
-    pass
+class BlurringMatrix:
+    matrix = None
+    pixels_per_degree = None
     
+    def __init__(self, matrix, pixels_per_degree=None):
+        self.matrix = matrix.astype('float64')
+        self.pixels_per_degree = pixels_per_degree
+    def calcPixelsPerDegree(self, resolution, display_size, visual_distance):
+        pixel_size = (float(display_size[0]) / resolution[0], float(display_size[1]) / resolution[1])
+        double_vdist = 2.0 * visual_distance
+        to_radians = 180.0/math.pi
+        degrees_per_pixel = (2.0*(math.atan(pixel_size[0]/double_vdist))*to_radians, 2.0*math.atan(pixel_size[1]/double_vdist)*to_radians)
+        
+        assert(degrees_per_pixel[0] == degrees_per_pixel[1])
+        
+        self.pixels_per_degree = 1.0 / degrees_per_pixel[0]
+        
+        return self #chainable method
+    def copy(self):
+        return BlurringMatrix(self.matrix.copy(), self.pixels_per_degree)
+    def resolutionIsCalculated(self):
+        return (self.pixels_per_degree is not None)
+    
+    """
+    Applies a low pass blurring filter, allowing a maximum of the given cycles per degree of visual angle.
+    """
+    def applyLowPassFilter(self, cyclesPerDegree):
+        if(not self.resolutionIsCalculated()):
+            raise RuntimeError("The pixels_per_degree must be set before a low-pass filter can be applied.")
+        
+        if(self.matrix.shape[2] != 3):
+            raise NotImplementedError("Filtering can only operate on RGB images (3-channel) at this time. Input has {0} channels.".format(self.matrix.shape[2]))
+        
+        rows, cols = self.matrix.shape[:2]
+        
+        (u, v) = dftuv(rows, cols)
+        D = numpy.sqrt(u**2 + v**2)
+        sigma = (self.pixels_per_degree * float(cyclesPerDegree)) / 2.0
+        f = numpy.exp(-(D**2)/((sigma**2)))
+        
+        fftR = numpy.fft.fft2(self.matrix[:,:,0])
+        fftG = numpy.fft.fft2(self.matrix[:,:,1])
+        fftB = numpy.fft.fft2(self.matrix[:,:,2])
+        
+        lowFFTr = fftR*f;
+        lowFFTg = fftG*f;
+        lowFFTb = fftB*f;
+        
+        out = numpy.zeros(self.matrix.shape)
+        out[:,:,0] = numpy.real(numpy.fft.ifft2(lowFFTr))
+        out[:,:,1] = numpy.real(numpy.fft.ifft2(lowFFTg))
+        out[:,:,2] = numpy.real(numpy.fft.ifft2(lowFFTb))
+        
+        return BlurringMatrix(numpy.clip(out, 0, 255).astype('float64'), self.pixels_per_degree)
+
+def dftuv(m, n):
+    u = numpy.arange(0, m)
+    v = numpy.arange(0, n)
+    
+    idx = (u > m/2.0).nonzero()
+    u[idx] = u[idx] - m
+    idy = (v > n/2.0).nonzero()
+    v[idy] = v[idy] - n
+    
+    [v,u] = numpy.meshgrid(v, u)
+    return (u, v)
